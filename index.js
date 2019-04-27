9666bb8f22fb3b2cb9e3ff188b81c39bb551d790
critical-path <37241479+critical-path@users.noreply.github.com>
2019-04-26 23:03:53 -0400

    On branch master
     Changes to be committed:
    
    	new file:   index.js
    	new file:   lib/ei48.js
    	new file:   lib/macaddress.js
    	new file:   lib/octet.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var ExtendedIdentifier48 = require("./lib/ei48.js")
+var MediaAccessControlAddress = require("./lib/macaddress.js")
+var Octet = require("./lib/octet.js")
+
+module.exports = {
+    ExtendedIdentifier48,
+    MediaAccessControlAddress,
+    Octet
+}
